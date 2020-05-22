d25e2b089006ff1482d9e2dbf371e71b58177739
liadkeller <liadkeller@gmail.com>
2020-05-22 15:51:43 +0300

    Add documentation to server code

--- server.py
+++ server.py
@@ -23,10 +23,15 @@ game = Game()
 
 
 def threaded_client(conn, this_player_id):
+    """
+    This function serves and handles one client
+    """
     global game
 
+    # recieves player request to join game
     this_player = None
-    while True: # player choose team
+    while True:
+        # recieves player team & role
         try:
             this_player_team, this_player_role = recv(conn)
         except Exception as e:
@@ -34,22 +39,26 @@ def threaded_client(conn, this_player_id):
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno, str(e))
             break
+
+        # checks if role is not taken
         for player in game.players:
             if player.online and player.role == this_player_role and player.team == this_player_team:
                 send_msg(conn, "role taken")
                 break
+
+        # if role is not taken, sends back "ok"
         else:
             send_msg(conn, "ok")
             this_player = game.add_new_player(this_player_team, this_player_role)
             break
 
-    reply = ""
+    # recieves and handles requests and notifications from user
     current_player_state = this_player.get_state(game)
     while this_player:
         try:
             data = recv(conn, blocking=False)
+
             if data:
-            
                 # captain stuff
                 if data == "captain get":
                     current_player_state = this_player.get_state(game)
@@ -84,14 +93,11 @@ def threaded_client(conn, this_player_id):
 
                 send_msg(conn, tuple(current_player_state))
 
-
             # update client for the new state if needed
             if this_player.get_state(game) != current_player_state:
                 send_msg(conn, "sending game state")
                 current_player_state = this_player.get_state(game)
                 send_msg(conn, tuple(current_player_state))
-
-            #time.sleep(1)
 
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
